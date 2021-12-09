4e072693fab6d01a1b70dc4a04f6b95f0d696c98
Ronny Pacheco <ronny.pach@gmail.com>
2021-12-09 00:50:04 -0500

    fixed formatting

--- exercises/ex5.py
+++ exercises/ex5.py
@@ -1,7 +1,7 @@
 name = 'Zed A. Shaw'
 age = 35 # prolly a lie
 height = 74  * 2.54# please do not use freedom units
-weight = 180 / 2.2046 # I wish
+weight = round(180 / 2.2046) # I wish
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
@@ -13,5 +13,5 @@ print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
 # this line is tricky, try to get it exactly right
 total = age + height + weight
-print(f"If I add {age}, {height}, and {weight} I get {total}.")
+print(f"If I add {age}, {height}, and {weight} I get {total:.2f}.")
 
